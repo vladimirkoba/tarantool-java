213ca3181043083179b44095cd851af7edda4e86
nicktorwald <nicktorwald@gmail.com>
2019-11-27 19:28:01 +0700

    msgpack: eliminate support for ByteBuffer
    MsgPackLite is not a public API and is used internally by Tarantool
    client to (de-)serialize iproto packets. Code analysis and code coverage
    shows that ByteBuffer is not passed as an argument to be serialized
    (see https://coveralls.io/builds/27255963/source?filename=src/main/java/org/tarantool/MsgPackLite.java#L172).
    To keep code simplified this code branch was removed as unused.

--- src/main/java/org/tarantool/MsgPackLite.java
+++ src/main/java/org/tarantool/MsgPackLite.java
@@ -164,21 +164,8 @@ public class MsgPackLite {
                 out.writeInt(data.length);
             }
             out.write(data);
-        } else if (item instanceof byte[] || item instanceof ByteBuffer) {
-            byte[] data;
-            if (item instanceof byte[]) {
-                data = (byte[]) item;
-            } else {
-                ByteBuffer bb = ((ByteBuffer) item);
-                if (bb.hasArray()) {
-                    data = bb.array();
-                } else {
-                    data = new byte[bb.capacity()];
-                    bb.position();
-                    bb.limit(bb.capacity());
-                    bb.get(data);
-                }
-            }
+        } else if (item instanceof byte[]) {
+            byte[] data = (byte[]) item;
             if (data.length <= MAX_8BIT) {
                 out.write(MP_BIN8);
                 out.writeByte(data.length);
